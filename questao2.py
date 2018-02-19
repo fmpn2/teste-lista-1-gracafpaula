df3e0cd444101aa1129de9ce79fc3c6e21df0f5a
gracafpaula <36635948+gracafpaula@users.noreply.github.com>
2018-02-19 12:26:21 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,1 +1,8 @@
 #peca ao usuario um numero e verifique se este numero eh par. imprima na tela o resultado da sua verificacao.
+
+
+numero = int(input("digite um numero"))
+if(numero%2==0):
+  print("numero eh par")
+else:
+  print("numero nao eh par")
